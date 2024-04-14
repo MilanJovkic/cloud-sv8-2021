f757f2a9d0cfb8e21dd6c795cdf9c993c0a308f2
MilanJovkic <milanjovkic@gmail.com>
2024-04-14 21:15:41 +0200

    added table name

--- src/main/java/com/example/movie/domain/Movie.java
+++ src/main/java/com/example/movie/domain/Movie.java
@@ -1,11 +1,9 @@
 package com.example.movie.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="sv_8_2021")
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
